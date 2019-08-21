3a9b47272dfa2c62d3a88938fb0f143b0f53bb64
yaoq <yaoq@qq.com>
2019-08-21 14:11:08 +0800

    更新分支内容

--- zjDemo/AppDelegate.m
+++ zjDemo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"git test");
+    NSLog(@"git test1");
     return YES;
 }
 
